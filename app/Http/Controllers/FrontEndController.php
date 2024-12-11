09fe4e56f26fceac06a4f468372966df392b0376
Your Name <you@example.com>
2024-12-11 22:29:01 +0500

    Page

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class FrontEndController extends Controller
 {
+    public function index(){
+        $endDate = Carbon::now()->addMonth()->startOfMonth()->addDays(4)->setTime(12, 0, 0);
+        return view('demo.index',compact('endDate'));
+    }
     public function dashboard(){
         return view('demo.dashboard');
     }
