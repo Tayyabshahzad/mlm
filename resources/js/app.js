9e8253debf3ea1ff3edaea8c43abbd6cd20ff626
Your Name <you@example.com>
2024-12-25 23:31:53 +0500

    Fix Build Issue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
 import '../css/app.css';
 import './bootstrap';
-
+import 'core-js/features/promise/all-settled';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import { initializeMetronic, debugMetronic  } from './metronic-init';
-
+import '@babel/polyfill';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
