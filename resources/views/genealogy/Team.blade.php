8ade61abe4dbd0e82e49ca8839a14bcacf0c313d
Your Name <you@example.com>
2025-01-04 19:50:17 +0500

    Add Space

--- resources/views/genealogy/Team.blade.php
+++ resources/views/genealogy/Team.blade.php
@@ -12,7 +12,7 @@
 @endsection
 
 @section('content')
-<!--begin::Content-->
+<!--begin::Content----------------------->
 <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
     <div class="subheader py-2 py-lg-6 subheader-solid" id="kt_subheader">
         <div class="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
