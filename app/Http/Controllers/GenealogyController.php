c021fc0215af9ee6b868dac3b9609cab92b6da4c
Your Name <you@example.com>
2025-01-04 19:48:46 +0500

    Add Space

--- app/Http/Controllers/GenealogyController.php
+++ app/Http/Controllers/GenealogyController.php
@@ -30,7 +30,7 @@ class GenealogyController extends Controller
             $buildHierarchy($descendant, $descendant->children ?? collect());
         }
     };
-    ////--
+    ////--git---
     $nodeDataArray[] = [
         'key' => $user->id,
         'name' => $user->name,
