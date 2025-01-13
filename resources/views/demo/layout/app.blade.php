e6bfe47b7a70dcb269a250a1f5007b05319d3935
Your Name <you@example.com>
2025-01-13 20:17:04 +0500

    Change 1

--- resources/views/demo/layout/app.blade.php
+++ resources/views/demo/layout/app.blade.php
@@ -3,6 +3,23 @@
 	<!--begin::Head-->
 	<head>
 		<base href="">
+
+		<meta charset="utf-8" />
+		<title>Global Visioners International | @yield('title') </title>
+		<meta name="description" content="Login page example" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<link rel="canonical" href="https://keenthemes.com/metronic" />
+		<!--begin::Fonts-->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+		<!--end::Fonts-->
+		<!--begin::Page Custom Styles(used by this page)-->
+		<link href="{{ asset('assets/css/pages/login/login-1.css')}}" rel="stylesheet" type="text/css" />
+		<!--end::Page Custom Styles-->
+		<!--begin::Global Theme Styles(used by all pages)-->
+		 
+		<link href="{{ asset('assets/css/themes/layout/aside/dark.css')}}" rel="stylesheet" type="text/css" />
+
+		
 		<meta charset="utf-8" />
 		<title> Global Visioners International | @yield('title')</title> 
 		<link rel="canonical" href="https://keenthemes.com/metronic" /> 
