5d6a2227f6034f5da5e547949dfebff01d7b2887
Your Name <you@example.com>
2025-01-13 19:30:14 +0500

    Change 1

--- resources/views/demo/layout/guest.blade.php
+++ resources/views/demo/layout/guest.blade.php
@@ -52,15 +52,7 @@
 					</div>
 					<!--end::Content body-->
 					<!--begin::Content footer-->
-					<div class="d-flex justify-content-lg-start justify-content-center align-items-end py-7 py-lg-0">
-						<div class="text-dark-50 font-size-lg font-weight-bolder mr-10">
-							<span class="mr-1">2020©</span>
-							<a href="http://keenthemes.com/metronic" target="_blank" class="text-dark-75 text-hover-primary">Keenthemes</a>
-						</div>
-						<a href="#" class="text-primary font-weight-bolder font-size-lg">Terms</a>
-						<a href="#" class="text-primary ml-5 font-weight-bolder font-size-lg">Plans</a>
-						<a href="#" class="text-primary ml-5 font-weight-bolder font-size-lg">Contact Us</a>
-					</div>
+					 
 					<!--end::Content footer-->
 				</div>
 				<!--end::Content-->
