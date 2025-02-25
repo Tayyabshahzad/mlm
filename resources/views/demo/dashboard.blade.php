2197c9375f2a9b126cc87a7e6d2c5f7ab905da04
Your Name <you@example.com>
2025-02-26 00:32:40 +0500

    Added Time

--- resources/views/demo/dashboard.blade.php
+++ resources/views/demo/dashboard.blade.php
@@ -27,7 +27,7 @@
                         <strong> {{ Auth::user()->current_pv_balance }} </strong> 
                     </div> 
                 </div> 
-                <span class="text-muted font-weight-bold ml-15">Time Zone </span>
+                <span class="text-muted font-weight-bold ml-15">Time & Time Zone </span>
                 <div class="kt-widget__content ml-5">
                     <div class="kt-widget__section">
                         <strong>  {{ now()->format('Y-m-d H:i:s') }} ({{ config('app.timezone') }}) </strong> 
