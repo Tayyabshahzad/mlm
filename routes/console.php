9296bc49777cf50ee60fd4f142f4ea6c3589ef5c
Your Name <you@example.com>
2025-03-03 00:36:36 +0500

    Set Job

--- routes/console.php
+++ routes/console.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
@@ -24,10 +25,31 @@ use Carbon\Carbon;
 
 
 
+// Schedule::command('roi:generate-weekly')
+// ->dailyAt('23:59')
+// ->when(function () {
+//     return now()->dayOfWeek !== \Carbon\Carbon::FRIDAY;
+// });
+
+Schedule::command('app:update-setting')->twiceDaily(10, 22);
+
+
 Schedule::command('roi:generate-weekly')
 ->dailyAt('23:59')
 ->when(function () {
-    return now()->dayOfWeek !== \Carbon\Carbon::FRIDAY;
-});
+    $isFriday = now()->dayOfWeek === Carbon::FRIDAY;
 
-Schedule::command('app:update-setting')->twiceDaily(10, 22);
+    if ($isFriday) {
+        Log::info('Skipped roi:generate-weekly command on Friday: ' . now());
+    } else {
+        Log::info('Executing roi:generate-weekly command on: ' . now());
+    }
+
+    return !$isFriday; // Run only if today is NOT Friday
+})
+->onSuccess(function () {
+    Log::info('roi:generate-weekly command executed successfully on: ' . now());
+})
+->onFailure(function () {
+    Log::error('roi:generate-weekly command failed on: ' . now());
+});
