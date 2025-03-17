acae767837165618d2edeb5d35c9a983605cc276
Your Name <you@example.com>
2025-03-17 07:43:38 +0500

    json update

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,5 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\ScheduleServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+    App\Providers\ScheduleServiceProvider::class, 
 ];
